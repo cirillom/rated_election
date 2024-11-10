69fed2e516bd071f66c36c5891a283e40a00be8e
Matheus Cirillo <matheuscirillo@usp.br>
2024-11-10 19:14:44 -0300

    cannot vote after election is closed rating is limited between 0 and 10

--- contracts/RatedElection.sol
+++ contracts/RatedElection.sol
@@ -1,18 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-// Uncomment this line to use console.log
-import "hardhat/console.sol";
-
 contract RatedElection {
     address public owner;
-
+    bool public electionClosed;
     struct Candidate {
         uint id;
         string name;
         uint totalRating;
     }
-
     uint public candidateCount;
     mapping(uint => Candidate) public candidates;
     mapping(address => bool) public authorizedVoters;
@@ -22,27 +18,32 @@ contract RatedElection {
         require(msg.sender == owner, "Not owner");
         _;
     }
-
     modifier onlyAuthorizedVoter() {
         require(authorizedVoters[msg.sender], "Not authorized");
+        _;
+    }
+    modifier electionNotClosed() {
+        require(!electionClosed, "Election has ended");
         _;
     }
 
     constructor() {
         owner = msg.sender;
+        electionClosed = false;
     }
 
-    function addCandidate(string memory _name) public onlyOwner {
+    function addCandidate(string memory _name) public onlyOwner electionNotClosed {
         candidateCount++;
         candidates[candidateCount] = Candidate(candidateCount, _name, 0);
     }
 
-    function addVoter(address _voter) public onlyOwner {
+    function addVoter(address _voter) public onlyOwner electionNotClosed{
         authorizedVoters[_voter] = true;
     }
 
-        function rateCandidate(uint _candidateId, uint _rating) public onlyAuthorizedVoter {
+    function rateCandidate(uint _candidateId, uint _rating) public onlyAuthorizedVoter electionNotClosed{
         require(_candidateId > 0 && _candidateId <= candidateCount, "Invalid candidate");
+        require(_rating <= 10, "Rating must be between 0 and 10");
         uint previousRating = voterRatings[msg.sender][_candidateId];
         candidates[_candidateId].totalRating = candidates[_candidateId].totalRating - previousRating + _rating;
         voterRatings[msg.sender][_candidateId] = _rating;
@@ -59,6 +60,7 @@ contract RatedElection {
         }
         require(_winnerId != 0, "No candidates");
         Candidate memory winner = candidates[_winnerId];
+        electionClosed = true;
         return (winner.id, winner.name, winner.totalRating);
     }
 
@@ -70,9 +72,7 @@ contract RatedElection {
 
     function getAllCandidates() public view returns (Candidate[] memory) {
         Candidate[] memory allCandidates = new Candidate[](candidateCount);
-        for(uint i = 1; i <= candidateCount; i++) {
-            allCandidates[i-1] = candidates[i];
-        }
+        for(uint i = 1; i <= candidateCount; i++) { allCandidates[i-1] = candidates[i]; }
         return allCandidates;
     }
 }
